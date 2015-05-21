ae367bccb96566f48ff68abba0087887a753c988
James Brooks <james@bluebaytravel.co.uk>
2015-05-21 08:45:58 +0100

    Disable check on nativeKeys

--- Affair.js
+++ Affair.js
@@ -186,7 +186,7 @@
         }
 
         if (!isObject(obj)) return [];
-        if (nativeKeys) return nativeKeys(obj);
+        // if (nativeKeys) return nativeKeys(obj);
         var keys = [];
         for (var key in obj) if (has(obj, key)) keys.push(key);
         return keys;
